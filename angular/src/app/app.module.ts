3e07c4451f8e82814a44449e47c75701b713458c
alexoliveira-ipira <alex.borgesolivveira@gmail.com>
2022-06-17 18:13:18 -0300

    Estruturas e Diretivas do Angular

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MarcatestoDirective } from './directives/marcatesto.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarcatestoDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/src/app/directives/marcatesto.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MarcatestoDirective } from './marcatesto.directive';
+
+describe('MarcatestoDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MarcatestoDirective();
+    expect(directive).toBeTruthy();
+  });
+});
