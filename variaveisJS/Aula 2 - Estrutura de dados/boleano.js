1bf413415c129361e3b0386a13eae1597aff23eb
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2022-06-24 01:27:00 -0300

    FlaxBox

--- variaveisJS/Aula 2 - Estrutura de dados/boleano.js
+++
@@ -1,7 +0,0 @@
-// Boolean
-// True ou Flase
-// Um valor boleano sempre vai trazer um valor verdadeiro ou falso
-// Sempre que tem uma esclamação o valor boleado fica invertido
-// Isso !false vai ser igual a true
-var valorVerdadeiro = 3 <= 0;
-console.log(valorVerdadeiro);
