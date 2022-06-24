c03a4c0abf823a4298636bc1abc231b816e5fefb
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2022-06-24 01:45:56 -0300

    JavaScript

--- TypeScript/src/Exemplos/Tipo de retorno da funcao.ts
+++
@@ -1,8 +0,0 @@
-// Deixando esplicito o que a função realmente retorna, não permitindo alterar os dados internos
-// dos tipos
-function somarValoresNumericos(numero1: number , numero2: number): number {
-    return numero1 + numero2    
-}
-
-console.log(somarValoresNumericos(1,3))
-
--- primeiroPasso/rodar o exemplo.md
+++
@@ -1,5 +0,0 @@
-# Rodando em container sem a necessidade de fazer nenhuma instalação
-
-## Comando docker
-
-docker run -d --rm -p 80:80 --name aulaphp2 -v "$PWD":/var/www/html php:7.2-apache
