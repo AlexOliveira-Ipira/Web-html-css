35cdf87fd2ce388e99456c770fa3f9d479f69500
AlexOliveira-Ipira <alex.borgesoliveira@gmail.com>
2022-06-24 00:40:13 -0300

    Criando TypeScript

--- modulosJS/Aula 2 - Pratica/main.mjs
+++
@@ -1,5 +0,0 @@
-import {mostraCidade , mostraIdade, mostraHobby} from "./funcoes.mjs";
-
-console.log(mostraIdade("Alex", 47))
-console.log(mostraCidade("Alex", "Consquista"))
-console.log(mostraHobby("Alex", "Pedalar"))
